e7f48e1442b7a906db6a3e5006795145788769f1
Bora İLCİ <borailci@outlook.com>
2024-09-16 11:24:27 -0400

    final commit

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -38,6 +38,7 @@ def step_impl(context):
     #
     rest_endpoint = f"{context.base_url}/products"
     context.resp = requests.get(rest_endpoint)
+    print(context.resp.status_code)
     assert(context.resp.status_code == HTTP_200_OK)
     for product in context.resp.json():
         context.resp = requests.delete(f"{rest_endpoint}/{product['id']}")
@@ -45,8 +46,14 @@ def step_impl(context):
 
     #
     # load the database with new products
-    #
+        
     for row in context.table:
-        #
-        # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
-        #
+        payload = {
+            "name": row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": row['available'] in ['True', 'true', '1'],
+            "category": row['category']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert context.resp.status_code == HTTP_201_CREATED
